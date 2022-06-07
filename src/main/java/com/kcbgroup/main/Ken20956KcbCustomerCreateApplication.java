504e7c435e4325e74c0d26636dd700461ef529d9
mary-wan <njengamaria29@gmail.com>
2022-06-07 11:43:36 +0300

    initial commit

---
+++ src/main/java/com/kcbgroup/main/Ken20956KcbCustomerCreateApplication.java
@@ -0,0 +1,13 @@
+package com.kcbgroup.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ken20956KcbCustomerCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ken20956KcbCustomerCreateApplication.class, args);
+	}
+
+}
